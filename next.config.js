8b13d480849df87d9fecec560bba018c7f8efab3
梁旭 <40330415+MangoLx@users.noreply.github.com>
2019-12-02 12:45:17 +0800

    chore:完善项目构建 (#1)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
+const withCss = require('@zeit/next-css');
 const withScss = require('@zeit/next-sass');
+const path = require('path');
 
-module.exports = withScss({
-  
-});
+module.exports = withCss(withScss({
+  // 在pages目录下那种后缀的文件会被认为是页面
+  pageExtensions: ['jsx', 'js'],
+
+  // webpack 相关配置
+  webpack(config, options) {
+    // 配置路径别名
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+      '@component': path.resolve(__dirname, 'component'),
+      '@pages': path.resolve(__dirname, 'page'),
+      '@utils': path.resolve(__dirname, 'utils'),
+    };
+    return config;
+  },
+}));
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+// 全局注入antd样式
+import App from 'next/app';
+import 'antd/dist/antd.css';
+
+export default App;
